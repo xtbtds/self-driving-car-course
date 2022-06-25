0c54a2cfdaf0a4ae1f476e4f3d5984705eee955f
xtbtds <liza.xtbtds@gmail.com>
2022-06-25 19:30:29 +0300

    second

--- road.js
+++ road.js
@@ -1,1 +1,41 @@
-const road = 1
+class Road {
+    constructor(x, width, laneCount=4) {
+        this.x=x;
+        this.laneCount=laneCount;
+        this.width=width;
+
+        this.left=x-width/2;
+        this.right=x+width/2;
+
+        const infinity=1000000;
+        this.top=-infinity;
+        this.bottom=infinity;
+    }
+
+    getLaneCenter(laneIndex){
+        const laneWidth = this.width / this.laneCount;
+        return this.left + laneIndex*laneWidth + laneWidth/2;
+    }
+
+    draw(ctx) {
+        ctx.lineWidth=5;
+        ctx.strokeStyle="white";
+
+        for (let i=0; i<=this.laneCount; i++) {
+            const x = lerp(
+                this.left,
+                this.right,
+                i/this.laneCount
+            )
+            if(i>0 && i<this.laneCount){
+                ctx.setLineDash([20,20]);
+            }else{
+                ctx.setLineDash([]);
+            }
+            ctx.beginPath();
+            ctx.moveTo(x, this.top);
+            ctx.lineTo(x, this.bottom);
+            ctx.stroke();
+        }
+    }
+}
