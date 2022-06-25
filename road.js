9593291a229aa96e1470861545205bcc572e325b
xtbtds <liza.xtbtds@gmail.com>
2022-06-25 20:08:49 +0300

    all car physics

--- road.js
+++ road.js
@@ -1,5 +1,5 @@
 class Road {
-    constructor(x, width, laneCount=4) {
+    constructor(x, width, laneCount=3) {
         this.x=x;
         this.laneCount=laneCount;
         this.width=width;
@@ -10,32 +10,45 @@ class Road {
         const infinity=1000000;
         this.top=-infinity;
         this.bottom=infinity;
+
+
+        const topLeft = {x:this.left, y:this.top};
+        const bottomLeft = {x:this.left, y:this.bottom};
+        const topRight = {x:this.right, y:this.top};
+        const bottomRight = {x:this.right, y:this.bottom};
+        this.borders = [
+            [topLeft, bottomLeft],
+            [topRight, bottomRight]
+        ];
     }
 
     getLaneCenter(laneIndex){
         const laneWidth = this.width / this.laneCount;
-        return this.left + laneIndex*laneWidth + laneWidth/2;
+        return this.left + Math.min(laneIndex, this.laneCount-1)*laneWidth + laneWidth/2;
     }
 
     draw(ctx) {
         ctx.lineWidth=5;
         ctx.strokeStyle="white";
 
-        for (let i=0; i<=this.laneCount; i++) {
+        for (let i=1; i<this.laneCount; i++) {
             const x = lerp(
                 this.left,
                 this.right,
                 i/this.laneCount
             )
-            if(i>0 && i<this.laneCount){
-                ctx.setLineDash([20,20]);
-            }else{
-                ctx.setLineDash([]);
-            }
+            ctx.setLineDash([20,20]);
             ctx.beginPath();
             ctx.moveTo(x, this.top);
             ctx.lineTo(x, this.bottom);
             ctx.stroke();
         }
+        ctx.setLineDash([]);
+        this.borders.forEach(border=>{
+            ctx.beginPath();
+            ctx.moveTo(border[0].x, border[0].y);
+            ctx.lineTo(border[1].x, border[1].y);
+            ctx.stroke();
+        });
     }
 }
